3b17aef10cdf2bb1b7910473445968587a562a09
rrng <renardrafferty@gmail.com>
2025-10-19 23:14:40 +0800

    oms_basic version

--- cpp/lib/trade/trade.hpp
+++ cpp/lib/trade/trade.hpp
@@ -14,17 +14,3 @@ namespace oms {
 
     using Trades = std::vector<Trade>;
 }
-
-// struct TradeInfo {
-//     std::uint64_t id_;
-//     Price price_;
-//     Size size_;
-//     Side side_;
-// };
-//
-// struct Trade {
-//     TradeInfo bid_;
-//     TradeInfo ask_;
-// };
-//
-// using Trades = std::vector<Trade>;
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -5,7 +5,5 @@
 #include <order.hpp>
 
 int main() {
-    std::cout << std::is_pod<oms::Order>::value << std::endl;
-    std::cout << std::is_trivially_copyable<oms::Order>::value << std::endl;
-    oms::Orderbook orderbook{};
+
 }
